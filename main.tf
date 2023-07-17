99d84efa4e38aa6944e31178d4e8fe5360cd4ead
Saar Ettinger <Saarett@users.noreply.github.com>
2023-07-17 19:21:26 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 resource "aws_s3_bucket" "foo-bucket" {
   region        = var.region
-    #checkov:skip=CKV_AWS_20:The bucket is a public static content host
-    #checkov:skip=CKV_AWS_144:Saar Test
   bucket        = local.bucket_name
   force_destroy = true
   acl           = "public-read"
